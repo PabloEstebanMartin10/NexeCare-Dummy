4be8da2ed3b928b63416dd3321ac71e78642bf2e
pma152402 <pma152402@gmail.com>
2025-11-24 12:09:23 +0100

    Navbar almost done, with TablerIcons

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,28 +1,43 @@
+import { IconHome, IconCalendar, IconUser } from "@tabler/icons-react";
+
 export default function Navbar() {
   return (
     <>
       {/* Navbar container */}
-      <div className="h-auto w-full border-b-2 px-5 pb-5 pt-2 border-neutral-300 fixed flex items-center bg-white shadow-md">
-
+      <div className="h-auto w-full border-b-2 px-5 pb-5 pt-2 border-neutral-300 fixed flex items-center bg-white shadow-md z-9999">
         {/* Logo IZQ */}
-        <div className="logo_container">
+        <div className="logo_container ml-7">
           <h2 className="font-bold">
             <span className="text-amber-600 text-6xl">Nexe</span>
             <span className="text-gray-600 text-6xl">Care</span>
             <img src=""></img>
           </h2>
           <h3 className="text-xs text-neutral-400 font-light">
-            <i>Simplificando el día a día de quienes más lo necesitan.</i>
+            <i>Simplificando la vida a quien más lo necesitan.</i>
           </h3>
         </div>
 
         {/* Enlaces DERECHA*/}
         <div className="flex gap-35 pt-2 ml-auto text-xl items-center text-neutral-600">
-          <a href="#" className=" transform transition-transform duration-200 hover:scale-110 hover:text-blue-400">Home</a>
-          <a href="#" className=" transform transition-transform duration-200 hover:scale-110 hover:text-blue-400">Agenda</a>
-          <a href="#" className="mr-10 transform transition-transform duration-200 hover:scale-110 hover:text-blue-400">Iniciar Sesión</a>
+          <a
+            href="#"
+            className=" transform transition-transform duration-200 hover:scale-110 hover:text-blue-400 flex flex-nowrap"
+          >
+            Home <span className="pl-3"><IconHome/></span>
+          </a>
+          <a
+            href="#"
+            className=" transform transition-transform duration-200 hover:scale-110 hover:text-blue-400 flex flex-nowrap"
+          >
+            Agenda  <span className="pl-3"><IconCalendar /></span>
+          </a>
+          <a
+            href="#"
+            className="mr-10 transform transition-transform duration-200 hover:scale-110 hover:text-blue-400 flex flex-nowrap"
+          >
+            Iniciar Sesión <span className="pl-3"><IconUser/></span>
+          </a>
         </div>
-
       </div>
     </>
   );
