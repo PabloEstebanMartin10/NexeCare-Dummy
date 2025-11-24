507a26b30c41f4293a1022d7364c67fbe5cf28ca
Pablo Esteban <estebanmartinpablo@gmail.com>
2025-11-24 10:57:45 +0100

    TreatmentController & TreatmentService get call done

--- backend/backend/src/main/java/com/NexeCare/backend/controller/TreatmentController.java
+++ backend/backend/src/main/java/com/NexeCare/backend/controller/TreatmentController.java
@@ -1,4 +1,33 @@
 package com.NexeCare.backend.controller;
 
+import com.NexeCare.backend.model.Treatment;
+import com.NexeCare.backend.service.TreatmentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("Treatments")
+@RequiredArgsConstructor
 public class TreatmentController {
+    private TreatmentService service;
+
+    @GetMapping("/{index}")
+    public ResponseEntity<Treatment> getTreatment(@PathVariable int index) {
+        Treatment treatment = service.getTreatment(index);
+        if (treatment == null) return ResponseEntity.noContent().build();
+        return ResponseEntity.ok(treatment);
+    }
+
+    @GetMapping("/getall")
+    public ResponseEntity<List<Treatment>> getAllTreatments() {
+        List<Treatment> treatments = service.getAllTreatments();
+        return ResponseEntity.ok(treatments);
+    }
+
 }
