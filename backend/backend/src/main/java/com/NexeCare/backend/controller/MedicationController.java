6ee133cb2db7b595e670277ee3a019a0580f4c50
Pablo Esteban <estebanmartinpablo@gmail.com>
2025-11-24 10:59:59 +0100

    Fix MedicationController param in get call

--- backend/backend/src/main/java/com/NexeCare/backend/controller/MedicationController.java
+++ backend/backend/src/main/java/com/NexeCare/backend/controller/MedicationController.java
@@ -4,10 +4,7 @@ import com.NexeCare.backend.model.Medication;
 import com.NexeCare.backend.service.MedicationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/medication")
@@ -17,7 +14,7 @@ public class MedicationController {
 
 
     @GetMapping("{index}")
-    public ResponseEntity<Medication> getMed(@RequestParam int index){
+    public ResponseEntity<Medication> getMed(@PathVariable int index) {
         Medication med = service.getMed(index);
         if (med == null) return ResponseEntity.noContent().build();
         return ResponseEntity.ok(med);
